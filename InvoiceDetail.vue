2f45f4dc8d6297bb32344994aa7610e6da449825
Adam Paplinski <adamp@MacBook-Pro.local>
2018-02-26 12:04:24 +0100

    updated datepicker, links to settings, moved reusable function to helpers module and did some styling improvements

--- InvoiceDetail.vue
+++ InvoiceDetail.vue
@@ -5,7 +5,7 @@
         <div v-if='!isLoading && vat_types.length==0'>
             <p>{{ trans('invoice.own_company_details_missing') }}</p>
             <p>
-                <router-link to='/myCompany' class="btn btn-primary">
+                <router-link to='/settings/own-company' class="btn btn-primary">
                     {{ trans('invoice.own_company_details_missing_btn') }}
                 </router-link>
             </p>
@@ -18,7 +18,7 @@
                 </div>
                 <div class='pull-right'>
                     <h4>{{ trans('invoice.date')}}:</h4>
-                    <input type='text' class='form-control datepickerClass' readonly="readonly" style='max-width:100px;'/>
+                    <datepicker v-model="invoice.date" :allowedDateRange="allowedDateRange" position='left'></datepicker>
                     <form-error :errors="vErrors" field="invoice.date"></form-error>
                 </div>
                 <div class="clearfix"></div>
@@ -35,17 +35,19 @@
                 <h4>
                     {{trans('invoice.billed_to')}}:
                 </h4>
-                <h3>
+                <p>
                     {{client.name}} - {{client.owner_name}} {{client.owner_surname}}
-                    <button type="button" class="btn btn-success btn-sm btn-icon bottom15 right15" @click='clientReset'>
+                    <button type="button" class="btn btn-info btn-sm btn-icon bottom15 right15" @click.prevent='clientReset'>
                         <span>{{trans('invoice.change_client_btn')}}</span>
                     </button>
-                </h3>
+                </p>
                 <address>
                     <span>
                         {{client.address_street}} {{client.address_house}}<br/>
                         {{client.address_postcode}} {{client.address_city}}<br/>
-                        {{client.address_country}}<br/>
+                        {{clientCountryName}}<br/>
+                        {{trans('client.reg_number_short')}}: {{client.reg_number}}<br/>
+                        {{trans('client.tax_code_short')}}: {{client.tax_code}}<br/>
                     </span>
                 </address>
             </div>
@@ -55,7 +57,18 @@
             <br/><br/>
             <div class="clearfix"></div>
             <div v-show="$store.state.settings.invoiceSettings.useBtwReverseCharge">
-                <input type="checkbox" v-model="invoice.isReverseCharge" /> {{trans('invoice.isBtwReverseCharge')}}
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="form-group">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" v-model="invoice.isReverseCharge" />
+                                    {{trans('invoice.isBtwReverseCharge')}}
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <h4>{{trans('invoice.invoice_lines_list_title')}}:</h4>
             <form-error :errors="vErrors" field="invoiceLines"></form-error>
@@ -74,11 +87,11 @@
                 <tbody>
                 <tr v-for="(invoiceLine, invoiceLineIndex) in invoiceLines">
                     <td >
-                        <button type="button" class="btn btn-accent" @click='removeLine(invoiceLineIndex)' style='width:100%'><i class="fa fa-remove"></i></button>
+                        <button type="button" class="btn btn-info" @click.prevent='removeLine(invoiceLineIndex)' style='width:100%'><i class="fa fa-remove"></i></button>
                     </td>
                     <td :data-label="trans('invoiceLines.name')">
                         <form-error :errors="vErrors" :field="'invoiceLines.'+invoiceLineIndex+'.name'"></form-error>
-                        <input type='text' v-model='invoiceLine.name' style='width:100%' class='form-control' />
+                        <input type='text' v-model='invoiceLine.name' style='width:100%' class='form-control' maxlength="50" />
                     </td>
                     <td :data-label="trans('invoiceLines.count')">
                         <form-error :errors="vErrors" :field="'invoiceLines.'+invoiceLineIndex+'.count'"></form-error>
@@ -107,34 +120,36 @@
                         {{trans('invoice.calculated_total')}}: {{ lineComputed[parseInt(invoiceLineIndex)].total.formatMoney() }}
                     </td>
                 </tr>
-
-                <tr class='summary'>
+                <tr class="summary">
                     <td :colspan='summaryColspan' data-label=''>
-                        <button type="button" class="btn btn-success btn-icon bottom15 right15" @click='addLine'>
+                        <button type="button" class="btn btn-info btn-icon bottom15 right15" @click='addLine'>
                             <i class="fa fa-plus"></i> &nbsp; <span>{{trans('invoice.invoice_line_add_btn')}}</span>
                         </button>
                     </td>
-                    <td>Subtotal:</td>
-                    <td>{{summaryComputed.subtotal.formatMoney()}}</td>
                 </tr>
-                <tr v-if="Vat[0]>=0" v-for="(Vat, i) in sortedVATs"  class='summary'>
-                    <td :colspan='summaryColspan'></td>
-                    <td>VAT {{Vat[0]}}% ({{Vat[2].formatMoney()}})</td>
-                    <td>{{ Vat[1].formatMoney() }}</td>
-                </tr>
-                <tr v-if="invoice.isReverseCharge" class='summary'>
-                    <td :colspan='summaryColspan'></td>
-                    <td colspan=2>{{trans('invoice.lineBtwReverseCharge')}} {{client.tax_code}}</td>
-                </tr>
-                <tr class='summary'>
-                    <td :colspan='summaryColspan'></td>
-                    <td><b>Total:</b></td>
-                    <td><b v-if="this.invoice.total">{{this.invoice.total.formatMoney()}}</b></td>
-                </tr>
+
                 </tbody>
             </table>
+            <table class="table invoiceSummaryTable">
+                <tr >
+                    <td class="title">Subtotal:</td>
+                    <td class="computedSummary">{{summaryComputed.subtotal.formatMoney()}}</td>
+                </tr>
+                <tr v-if="Vat[0]>=0" v-for="(Vat, i) in sortedVATs">
+                    <td class="title">VAT {{Vat[0]}}% ({{Vat[2].formatMoney()}})</td>
+                    <td class="computedSummary">{{ Vat[1].formatMoney() }}</td>
+                </tr>
+                <tr v-if="invoice.isReverseCharge">
+                    <td class="title"></td>
+                    <td class="computedSummary">{{trans('invoice.lineBtwReverseCharge')}} {{client.tax_code}}</td>
+                </tr>
+                <tr>
+                    <td class="title"><b>Total:</b></td>
+                    <td class="computedSummary"><b v-if="this.invoice.total">{{this.invoice.total.formatMoney()}}</b></td>
+                </tr>
+            </table>
             <h4>{{trans('invoice.notes')}}:</h4>
-            <textarea v-model='invoice.notes' class="form-control" rows="3"></textarea>
+            <autosize-textarea v-model='invoice.notes' class="form-control" maxlength="127"></autosize-textarea>
             <form-error :errors="vErrors" field="invoice.notes"></form-error>
 
             <h4>{{trans('invoice.data_paymentDays')}}:</h4>
@@ -143,11 +158,11 @@
             <br/>
             <br/>
             <hr/>
-            <button type="button" class="btn btn-success btn-icon bottom15 right15" @click='saveInvoice(false)'>
+            <button type="button" class="btn btn-primary btn-icon bottom15 right15" @click='saveInvoice(false)'>
                 <i class="fa fa-save"></i> &nbsp; <span>{{trans('invoice.invoice_save_btn')}}</span>
             </button>
 
-            <button type='button' class="btn btn-primary btn-icon bottom15 right15" @click='downloadPDF' >
+            <button type='button' class="btn btn-info btn-icon bottom15 right15" @click='downloadPDF' >
                 <i class="fa fa-download"></i> &nbsp; <span>{{trans('invoice.invoice_PDF_btn')}}</span>
             </button>
 
@@ -161,14 +176,22 @@
     import helpers from './../helpers.js';
     import LoadingBox from './baseComponents/LoadingBox';
     Vue.component('LoadingBox', LoadingBox);
-
+    import FormError from './FormError.vue';
+    Vue.component('form-error', FormError);
+    import AutocompleteClients from './AutocompleteClients.vue';
+    Vue.component('autocomplete-clients', AutocompleteClients);
+    import AutosizeTextarea from './elements/AutosizeTextarea.vue';
+    Vue.component('autosize-textarea', AutosizeTextarea);
+    import Datepicker from './elements/Datepicker';
+    Vue.component('datepicker', Datepicker);
+    
     export default {
         components: { Money },
         mounted() {
             this.fetchInvoiceData();
             this.getConfig();
 
-            var self = this;
+            let self = this;
             self.isLoading++;
             this.$store.dispatch('getMyInvoiceSettings')
                 .then(
@@ -198,6 +221,14 @@
             }
         },
         computed:{
+            clientCountryName(){
+                if(this.client.address_country){
+                    return trans('countries.'+this.client.address_country);
+                }
+                else{
+                    return '';
+                }
+            },
             defaultVatType(){
                 let dVat = this.vat_types.filter(vat => vat.is_default==1);
                 /* get default VAT or first found VAT or null */
@@ -219,17 +250,17 @@
                 return this.computeInvoiceLines();
             },
             summaryComputed: function(){
-                var subtotal = 0;
-                var VATs = []; // [[vat%, vatAmount,nettoAmount], ...]
+                let subtotal = 0;
+                let VATs = []; // [[vat%, vatAmount,nettoAmount], ...]
 
-                var invoiceTotal = 0;
+                let invoiceTotal = 0;
                 this.invoiceLines.map(function(line, index) {
                     if( this.invoice.isReverseCharge ){
                         line.vat = 0;
                     }
-                    var lineNetto = line.unit_cost * line.count;
-                    var lineVat = (line.vat>=0) ? lineNetto * line.vat / 100 : 0;
-                    var lineTotal = this.round(lineNetto + lineVat,2);
+                    let lineNetto = line.unit_cost * line.count;
+                    let lineVat = (line.vat>=0) ? lineNetto * line.vat / 100 : 0;
+                    let lineTotal = helpers.round(lineNetto + lineVat,2);
                     invoiceTotal += lineTotal;
                     subtotal += lineNetto;
                     if(this.isShowVatColumn){
@@ -237,12 +268,11 @@
                     }
                 }.bind(this));
                 this.invoice.total = invoiceTotal;
-                this.log();
                 return {subtotal: subtotal, VATs: VATs};
             },
             sortedVATs: function(){
                 return this.summaryComputed.VATs.sort(function(a, b) {
-                    var x = a[0]; var y = b[0];
+                    let x = a[0]; let y = b[0];
                     return ((x < y) ? -1 : ((x > y) ? 1 : 0));
                 });
             }
@@ -258,16 +288,9 @@
                 this.client = {};
                 this.$forceUpdate();
             },
-            isNaN: function(val, def){
-                return !isNaN(val) ? val : def;
-            },
-            log: function(){
-                console.log(this.invoice);
-                console.log(this.invoiceLines);
-            },
             fetchInvoiceData: function () {
                 this.isLoading++;
-                var self = this;
+                let self = this;
                 axios.get('/api/invoices/'+this.invoice_id)
                     .then(function (rsp) {
                         self.invoice.total = 0;
@@ -275,11 +298,11 @@
                         self.allowedDateRange = rsp.data.allowedDateRange;
                         self.invoiceLines = rsp.data.invoiceLines;
                         self.client = rsp.data.client;
-                        self.isLoading--;
+
                         if(self.invoice_id==0 && self.invoiceLines.length==0){
                             self.addLine();
                         }
-                        self._initializeCalendar();
+                        self.isLoading--;
                     })
                     .catch(function (error) {
                         console.log(error);
@@ -287,7 +310,7 @@
                     });
             },
             getConfig: function () {
-                var self = this;
+                let self = this;
                 self.isLoading++;
                 axios.get('/api/config',{params: { unit_types:{},vat_types:{} }})
                     .then(function (rsp) {
@@ -304,11 +327,10 @@
                 this.invoiceLines.push( this.defaultInvoiceLine() );
             },
             removeLine: function(index){
-                console.log('removing line'+index);
                 this.invoiceLines.splice( index, 1 );
             },
             saveInvoice: function(andDownloadPDF){
-                var self = this;
+                let self = this;
                 axios.post(
                     '/api/invoices/store',
                     {
@@ -316,10 +338,9 @@
                         invoiceLines: this.invoiceLines
                     })
                     .then(function (response) {
-                        console.log('Response save: ',response);
                         self.vErrors = {};
                         if(andDownloadPDF){
-                            window.location = '/api/pdf/download?id=' + self.invoice.id + '&token=' + localStorage.getItem('id_token');
+                            window.location = '/api/pdf/download/invoice?id=' + response.data.invoice_id + '&token=' + localStorage.getItem('id_token');
                         }
                         EventBus.$emit('invoices.changed', self.invoice);
                         self.$emit('close');
@@ -334,24 +355,24 @@
                             helpers.scrollToError();
                         }
                     });
-                },
+            },
             downloadPDF: function(){
                 this.saveInvoice( true );
             },
             computeInvoiceLines: function(){
-                var c = {};
+                let c = {};
                 this.invoiceLines.map(function(line, index) {
                     c[index] = {};
-                    var lineNetto = line.unit_cost * line.count;
-                    var lineVat = (line.vat>=0) ? lineNetto * line.vat / 100 : 0;
-                    var lineTotal = this.round(lineNetto + lineVat,2);
+                    let lineNetto = line.unit_cost * line.count;
+                    let lineVat = (line.vat>=0) ? lineNetto * line.vat / 100 : 0;
+                    let lineTotal = helpers.round(lineNetto + lineVat,2);
 
-                    c[index].netto = this.isNaN( this.round(lineNetto,2), '--');
-                    c[index].vat = this.isNaN( this.round(lineVat,2), '--');
-                    c[index].total = this.isNaN( this.round(lineTotal,2), '--');
+                    c[index].netto = helpers.isNaN( helpers.round(lineNetto,2), '--');
+                    c[index].vat = helpers.isNaN( helpers.round(lineVat,2), '--');
+                    c[index].total = helpers.isNaN( helpers.round(lineTotal,2), '--');
                 }.bind(this));
                 return c;
-                },
+            },
             defaultInvoiceLine: function(){
                 return {
                     name: '',
@@ -361,11 +382,9 @@
                     vat: this.defaultVatType
                 }
             },
-            round: function(num, decimals){
-                return Math.round(num*100)/100;
-                },
+            
             _updateVAT: function(VATs, percent, amountVAT, amountNetto){
-                var found = false;
+                let found = false;
                 VATs.map( function(Vat,i){
                     if (Vat[0] == percent){
                         VATs[i][1] += amountVAT;
@@ -378,25 +397,6 @@
                 }
                 return VATs;
             },
-            _initializeCalendar(){
-                var self = this;
-                var dp = $('.datepickerClass').datepicker({
-                    format: 'yyyy-mm-dd',
-                    autoclose: true,
-                    startDate: (self.allowedDateRange.from) ? new Date(self.allowedDateRange.from) : null,
-                    endDate: (self.allowedDateRange.to) ? new Date(self.allowedDateRange.to) : null,
-                })
-                    .on('changeDate', function(ev){
-                        var year = ev.date.getFullYear();
-                        var month = ev.date.getMonth()+1;
-                        month = month<10 ? '0'+month : month;
-                        var day = ev.date.getDate();
-                        day = day<10 ? '0'+day : day;
-                        self.invoice.date = year+'-'+month+'-'+day; // always useful format
-                        //$('.datepickerClass').datepicker('setDate', new Date( ev.date ) );
-                    });
-                $('.datepickerClass').datepicker('setDate', new Date(self.invoice.date) ); // initial value
-            }
         }
     }
 </script>
